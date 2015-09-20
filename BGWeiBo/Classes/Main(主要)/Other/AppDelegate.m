42566394201ce9fd172c2ba81878870f59ad646a
fengwujie <fengwujie_love@163.com>
2015-09-20 19:26:15 +0800

    OAuth授权
    加载登录页面和获得accessToken

--- BGWeiBo/Classes/Main(主要)/Other/AppDelegate.m
+++ BGWeiBo/Classes/Main(主要)/Other/AppDelegate.m
@@ -9,6 +9,7 @@
 #import "AppDelegate.h"
 #import "BGTabBarViewController.h"
 #import "BGNewfeatureViewController.h"
+#import "BGOAuthViewController.h"
 
 @interface AppDelegate ()
 
@@ -24,8 +25,26 @@
     
     // 2.设置根控制器
     //self.window.rootViewController = [[BGTabBarViewController alloc] init];
-    self.window.rootViewController = [[BGNewfeatureViewController alloc] init];
     
+//    // 判断是否需要显示新特性
+//    //self.window.rootViewController = [[BGNewfeatureViewController alloc] init];
+//    NSString *key = @"CFBundleVersion";
+//    // 上一次的使用版本（存储在沙盒中的版本号）
+//    NSString *lastVersion = [[NSUserDefaults standardUserDefaults] objectForKey:key];
+//    // 当前软件的版本号（从Info.plist中获得）
+//    NSString *currentVersion = [NSBundle mainBundle].infoDictionary[key];
+//
+//    if ([currentVersion isEqualToString:lastVersion]) { // 版本号相同：这次打开和上次打开的是同一个版本
+//        self.window.rootViewController = [[BGTabBarViewController alloc] init];
+//    } else { // 这次打开的版本和上一次不一样，显示新特性
+//        self.window.rootViewController = [[BGNewfeatureViewController alloc] init];
+//
+//        [[NSUserDefaults standardUserDefaults] setObject:currentVersion forKey:key];
+//        [[NSUserDefaults standardUserDefaul
+//        // 将当前的版本号存进沙盒ts] synchronize];
+//    }
+    
+    self.window.rootViewController = [[BGOAuthViewController alloc] init];
     
     // 显示根控制器
     [self.window makeKeyAndVisible];
---
+++ BGWeiBo/Classes/OAuth(授权)/Controller/BGOAuthViewController.m
@@ -0,0 +1,119 @@
+//
+//  BGOAuthViewController.m
+//  BGWeiBo
+//
+//  Created by apple on 14-10-10.
+//  Copyright (c) 2014年 heima. All rights reserved.
+//
+
+#import "BGOAuthViewController.h"
+#import "AFNetworking.h"
+
+@interface BGOAuthViewController () <UIWebViewDelegate>
+
+@end
+
+@implementation BGOAuthViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // 1.创建一个webView
+    UIWebView *webView = [[UIWebView alloc] init];
+    webView.frame = self.view.bounds;
+    webView.delegate = self;
+    [self.view addSubview:webView];
+    
+    // 2.用webView加载登录页面（新浪提供的）
+    // 请求地址：https://api.weibo.com/oauth2/authorize
+    /* 请求参数：
+     client_id	true	string	申请应用时分配的AppKey。
+     redirect_uri	true	string	授权回调地址，站外应用需与设置的回调地址一致，站内应用需填写canvas page的地址。
+    */
+    /*
+     Xcode7，访问https:会出现错误：NSURLSession/NSURLConnection HTTP load failed (kCFStreamErrorDomainSSL, -9802)
+     原因是安全机制改了，需要在info文件中增加配置
+     <key>NSAppTransportSecurity</key>
+     <dict>
+     <key>NSAllowsArbitraryLoads</key>
+     <true/>
+     </dict>
+     */
+    NSURL *url = [NSURL URLWithString:@"https://api.weibo.com/oauth2/authorize?client_id=497493438&redirect_uri=http://www.baidu.com"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [webView loadRequest:request];
+}
+
+#pragma mark - webView代理方法
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+//    BGLog(@"----webViewDidFinishLoad");
+}
+
+- (void)webViewDidStartLoad:(UIWebView *)webView
+{
+//    BGLog(@"----webViewDidStartLoad");
+}
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    // 1.获得url
+    NSString *url = request.URL.absoluteString;
+    BGLog(@"%@", url);
+    // 2.判断是否为回调地址
+    NSRange range = [url rangeOfString:@"code="];
+    if (range.length != 0) { // 是回调地址
+        // 截取code=后面的参数值
+        int fromIndex = range.location + range.length;
+        NSString *code = [url substringFromIndex:fromIndex];
+        
+        BGLog(@"%@", code);
+        
+        // 利用code换取一个accessToken
+        [self accessTokenWithCode:code];
+    }
+    
+    return YES;
+}
+
+/**
+ *  利用code（授权成功后的request token）换取一个accessToken
+ *
+ *  @param code 授权成功后的request token
+ */
+- (void)accessTokenWithCode:(NSString *)code
+{
+/*
+ URL：https://api.weibo.com/oauth2/access_token
+ 
+ 请求参数：
+ client_id：申请应用时分配的AppKey
+ client_secret：申请应用时分配的AppSecret
+ grant_type：使用authorization_code
+ redirect_uri：授权成功后的回调地址
+ code：授权成功后返回的code
+ */
+    // 1.请求管理者
+    AFHTTPRequestOperationManager *mgr = [AFHTTPRequestOperationManager manager];
+//    mgr.responseSerializer = [AFJSONResponseSerializer serializer];
+    
+    // AFN的AFJSONResponseSerializer默认不接受text/plain这种类型
+    // 修改AFURLResponseSerialization.m文件，self.acceptableContentTypes = [NSSet setWithObjects:@"text/plain",@"application/json", @"text/json", @"text/javascript", nil];
+    
+    // 2.拼接请求参数
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"client_id"] = @"497493438";
+    params[@"client_secret"] = @"2b7ffa72da4c6cba326b6803cbf91cd1";
+    params[@"grant_type"] = @"authorization_code";
+    params[@"redirect_uri"] = @"http://www.baidu.com";
+    params[@"code"] = code;
+    
+    // 3.发送请求
+    [mgr POST:@"https://api.weibo.com/oauth2/access_token" parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        BGLog(@"请求成功-%@", responseObject);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        BGLog(@"请求失败-%@", error);
+    }];
+}
+@end
